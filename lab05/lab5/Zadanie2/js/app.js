3cf50d2861dcd34419e9d884b942944acd0ab5fd
Mikolaj <mkolek@student.agh.edu.pl>
2025-11-07 12:11:18 +0100

    lab05 zad2 simpler solution

--- lab05/lab5/Zadanie2/js/app.js
+++ lab05/lab5/Zadanie2/js/app.js
@@ -3,14 +3,7 @@ const message = document.querySelector('#message');
 
 if (toggleButton && message) {
   toggleButton.addEventListener('click', () => {
-    const isHidden = message.classList.contains('hide');
-
-    if (isHidden) {
-      message.classList.remove('hide');
-      message.classList.add('reveal');
-    } else {
-      message.classList.remove('reveal');
-      message.classList.add('hide');
-    }
+    const isHidden = message.classList.toggle('hide');
+    message.classList.toggle('reveal', !isHidden);
   });
 }
